e477b3038954da0c1f39d990eb438e9645fb6e84
Dan Taylor <dt.raelcun@gmail.com>
2014-09-20 22:56:02 -0400

    carousel is now being dynamically created

--- js/controllers/home/homeCarouselCtrl.js
+++ js/controllers/home/homeCarouselCtrl.js
@@ -1,5 +1,33 @@
 angular.module('mainApp')
 .controller('homeCarouselCtrl', function($scope) {
   $scope.slides = [
+    {
+      'image_path': 'img/31.png',
+      'image_thumb': 'img/31s.png',
+      'title': 'About',
+      'desc': 'Lorem ipsum dolor sit',
+      'show_nav': true
+    },
+    {
+      'image_path': 'img/32.png',
+      'image_thumb': 'img/32s.png',
+      'title': 'Projects',
+      'desc': 'Lorem ipsum dolor sit',
+      'show_nav': true
+    },
+    {
+      'image_path': 'img/31.png',
+      'image_thumb': 'img/31s.png',
+      'title': 'About',
+      'desc': 'Lorem ipsum dolor sit',
+      'show_nav': false
+    },
+    {
+      'image_path': 'img/32.png',
+      'image_thumb': 'img/32s.png',
+      'title': 'Projects',
+      'desc': 'Lorem ipsum dolor sit',
+      'show_nav': false
+    }
   ];
 });
